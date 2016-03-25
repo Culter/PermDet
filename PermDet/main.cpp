365e7f9f36c85fd766d16532c1842a54ade51044
culter <culter@culter-macbookpro.local>
2016-03-24 21:57:06 -0700

    Initial Commit

---
+++ PermDet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PermDet
+//
+//  Created by culter on 3/24/16.
+//  Copyright © 2016 culter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
